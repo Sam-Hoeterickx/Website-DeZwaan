343d4ab2e062ce615183309f031b6bc18fcb86ea
Sam Hoeterickx <sam.hoeterickx@student.ehb.be>
2024-03-11 15:17:01 +0100

    first commit

---
+++ Javascript/data.js
@@ -0,0 +1,286 @@
+export function getBierenVat(){
+    return JSON.stringify(
+        [
+            {
+                "name": "Stella Artois - 25cl/33cl",
+                "price":"€2,20 / €2,40"
+            },
+            {
+                "name": "Primus - 25cl/33cl",
+                "price":"€2,20 / €2,40"
+            },
+            {
+                "name": "Hoegaarden - 25cl/33cl",
+                "price":"€2,20 / €2,40"
+            },
+            {
+                "name": "Bolleke",
+                "price":"€2,20"
+            },
+            {
+                "name": "Bier van de maand",
+                "price":"€..."
+            }
+        ]
+    );
+}
+export function getBierenFles(){
+    return JSON.stringify(
+        [
+            {
+                "name": "Jupiler",
+                "price":"€2,20"
+            },
+            {
+                "name": "Lindemans",
+                "price":"€2,70"
+            },
+            {
+                "name": "Liefmans",
+                "price":"€2,70"
+            },
+            {
+                "name": "Rodenbach",
+                "price":"€2,70"
+            },
+            {
+                "name": "Leffe Blond",
+                "price":"€3,70"
+            },
+            {
+                "name": "Leffe Bruin",
+                "price":"€3,70"
+            },
+            {
+                "name": "Leffe Blond/Bruin NA",
+                "price":"€3,70"
+            },
+            {
+                "name": "Stella NA",
+                "price":"€2,20"
+            },
+            {
+                "name": "Duvel",
+                "price":"€3,90"
+            },
+            {
+                "name": "Westmalle Tripel",
+                "price":"€4,00"
+            },
+            {
+                "name": "Westmalle Dubbel",
+                "price":"€4,00"
+            },
+            {
+                "name": "La Chouffe",
+                "price":"€4,00"
+            },
+            {
+                "name": "Cherry Chouffe",
+                "price":"€4,00"
+            },
+            {
+                "name": "Paix Dieux",
+                "price":"€4,70"
+            },
+            {
+                "name": "Orval",
+                "price":"€4,00"
+            },
+            {
+                "name": "Tripel D'Anvers",
+                "price":"€4,00"
+            },
+            {
+                "name": "Chimay Blauw",
+                "price":"€4,10"
+            }
+        ]
+    );
+}
+export function getWijnen(){
+    return JSON.stringify(
+        [
+            {
+                "name": "Witte Wijn (Droog)",
+                "price":"€3,60"
+            },
+            {
+                "name": "Witte Wijn (Zoet)",
+                "price":"€3,80"
+            },
+            {
+                "name": "Rosé Wijn",
+                "price":"€3,60"
+            },
+            {
+                "name": "Rode Wijn",
+                "price":"€3,60"
+            },
+            {
+                "name": "Cava",
+                "price":"€3,80"
+            },
+            {
+                "name": "Porto Wit/Rood",
+                "price":"€3,40"
+            }
+        ]
+    );
+}
+export function getWarmeDranken(){
+    return JSON.stringify(
+        [
+            {
+                "name": "Koffie/Deca",
+                "price":"€2,50"
+            },
+            {
+                "name": "Mokka",
+                "price":"€2,50"
+            },
+            {
+                "name": "Thee",
+                "price":"€2,50"
+            },
+            {
+                "name": "Cecemel",
+                "price":"€2,80"
+            },
+            {
+                "name": "Cafe Laté",
+                "price":"€3,50"
+            },
+            {
+                "name": "Soep",
+                "price":"€2,50"
+            }
+        ]
+    );
+}
+export function getSterkeDrank(){
+    return JSON.stringify(
+        [
+            {
+                "name": "Graanjenever",
+                "price":"€2,50"
+            },
+            {
+                "name": "Jagermeister",
+                "price":"€2,70"
+            },
+            {
+                "name": "Cognac",
+                "price":"€5,00"
+            },
+            {
+                "name": "Gin Bombay",
+                "price":"€5,00"
+            },
+            {
+                "name": "Baileys",
+                "price":"€5,00"
+            },
+            {
+                "name": "Wiskey",
+                "price":"€5,00"
+            },
+            {
+                "name": "Wodka Wit",
+                "price":"€5,00"
+            },
+            {
+                "name": "Bacardi",
+                "price":"€5,00"
+            },
+            {
+                "name": "Amaretto",
+                "price":"€5,00"
+            },
+            {
+                "name": "Martini",
+                "price":"€5,00"
+            }
+        ]
+    );
+}
+export function getSnacks(){
+    return JSON.stringify(
+        [
+            {
+                "name": "Chips",
+                "price":"€2,00"
+            },
+            {
+                "name": "Bifi Worst",
+                "price":"€2,00"
+            }
+        ]
+    );
+}
+export function getFrisdrank(){
+    return JSON.stringify(
+        [
+            {
+                "name": "Cola / Cola Zero",
+                "price":"€2,20"
+            },
+            {
+                "name": "Fanta",
+                "price":"€2,20"
+            },
+            {
+                "name": "Sprite",
+                "price":"€2,20"
+            },
+            {
+                "name": "Val Bruis / Plat",
+                "price":"€2,20"
+            },
+            {
+                "name": "Agrum",
+                "price":"€2,20"
+            },
+            {
+                "name": "Bitter Lemon",
+                "price":"€2,20"
+            },
+            {
+                "name": "Ice-Tea",
+                "price":"€2,20"
+            },
+            {
+                "name": "Cecemel",
+                "price":"€2,40"
+            },
+            {
+                "name": "Looza Orange / Pompelmoes",
+                "price":"€2,40"
+            },
+            {
+                "name": "Tonisteiner Orange",
+                "price":"€2,30"
+            },
+            {
+                "name": "Tonisteiner Citroen",
+                "price":"€2,40"
+            },
+            {
+                "name": "Tonisteiner Sprudel",
+                "price":"€2,20"
+            },
+            {
+                "name": "Tonisteiner Naranja",
+                "price":"€2,60"
+            },
+            {
+                "name": "Tonisteiner Vruchtenkorf",
+                "price":"€2,60"
+            },
+            {
+                "name": "Tonisteiner Gember",
+                "price":"€2,60"
+            }
+        ]
+    )
+}
---
+++ Javascript/main.js
@@ -0,0 +1,157 @@
+import{
+    getBierenFles,
+    getBierenVat,
+    getSnacks,
+    getWijnen,
+    getSterkeDrank,
+    getWarmeDranken,
+    getFrisdrank
+}from "./data.js";
+
+let bierenVat;
+let bierenFles;
+let wijnen;
+let snacks;
+let sterkeDranken;
+let warmedranken;
+let frisdranken;
+
+init();
+
+function init(){
+    bierenVat = JSON.parse(getBierenVat());
+    bierenFles = JSON.parse(getBierenFles());
+    wijnen = JSON.parse(getWijnen());
+    snacks = JSON.parse(getSnacks());
+    sterkeDranken = JSON.parse(getSterkeDrank());
+    warmedranken = JSON.parse(getWarmeDranken());
+    frisdranken = JSON.parse(getFrisdrank());
+
+    console.log(bierenVat);
+    console.log(bierenFles);
+    console.log(wijnen);
+    console.log(snacks);
+    console.log(sterkeDranken);
+    console.log(warmedranken)
+    console.log(frisdranken);
+
+    renderBierenVat();
+    renderBierenfles();
+    renderWijnen();
+    renderSnacks();
+    renderSterkeDranken();
+    renderWarmeDranken();
+    renderFrisDranken();
+}
+
+function renderBierenVat(){
+    let htmlString = "";
+    const containerBierVat = document.querySelector('#bieren-vat');
+
+    bierenVat.forEach(function(bierVat){
+        htmlString += `
+            <div class="article">
+                <p>${bierVat.name}</p>
+                <p>${bierVat.price}</p>
+            </div>
+        `
+        console.log(bierVat);
+    });
+    containerBierVat.innerHTML += htmlString;   
+}
+
+function renderBierenfles(){
+    let htmlString = "";
+    const containerBierFles = document.querySelector('#bieren-fles');
+
+    bierenFles.forEach(function(bierFles){
+        htmlString += `
+        <div class="article">
+            <p>${bierFles.name}</p>
+            <p>${bierFles.price}</p>
+        </div>
+        `
+        console.log(bierFles);
+    });
+    containerBierFles.innerHTML += htmlString;   
+}
+
+function renderWijnen(){
+    let htmlString = "";
+    const containerWijnen = document.querySelector('#wijnen');
+
+    wijnen.forEach(function(wijn){
+        htmlString += `
+        <div class="article">
+            <p>${wijn.name}</p>
+            <p>${wijn.price}</p>
+        </div>
+        `
+        console.log(wijnen);
+    });
+    containerWijnen.innerHTML += htmlString;   
+}
+
+function renderSnacks(){
+    let htmlString = "";
+    const containerSnacks = document.querySelector('#snacks');
+
+    snacks.forEach(function(snack){
+        htmlString += `
+        <div class="article">
+            <p>${snack.name}</p>
+            <p>${snack.price}</p>
+        </div>
+        `
+        console.log(snack);
+    });
+    containerSnacks.innerHTML += htmlString;   
+}
+
+function renderSterkeDranken(){
+    let htmlString = "";
+    const containerSterkeDranken = document.querySelector('#sterkedrank');
+
+    sterkeDranken.forEach(function(sterkeDrank){
+        htmlString += `
+        <div class="article">
+            <p>${sterkeDrank.name}</p>
+            <p>${sterkeDrank.price}</p>
+        </div>
+        `
+        console.log(sterkeDrank);
+    });
+    containerSterkeDranken.innerHTML += htmlString;   
+}
+
+function renderWarmeDranken(){
+    let htmlString = "";
+    const containerWarmeDranken = document.querySelector('#warmedrank');
+
+    warmedranken.forEach(function(warmedrank){
+        htmlString += `
+        <div class="article">
+            <p>${warmedrank.name}</p>
+            <p>${warmedrank.price}</p>
+        </div>
+        `
+        console.log(warmedrank);
+    });
+    containerWarmeDranken.innerHTML += htmlString;   
+}
+
+function renderFrisDranken(){
+    let htmlString = "";
+    const containerFrisDranken = document.querySelector('#frisdrank');
+
+    frisdranken.forEach(function(frisdrank){
+        htmlString += `
+        <div class="article">
+            <p>${frisdrank.name}</p>
+            <p>${frisdrank.price}</p>
+        </div>
+        `
+        console.log(frisdrank);
+    });
+    containerFrisDranken.innerHTML += htmlString;   
+}
